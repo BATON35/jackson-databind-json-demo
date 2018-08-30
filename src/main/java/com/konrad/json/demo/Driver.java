d1359d82db5e11e7c777f2b91911df37903e78fa
Konrad Bartczak <konradbartczak@onet.pl>
2018-08-30 22:04:24 +0200

    fixed dependence for jackson

--- src/main/java/com/konrad/json/demo/Driver.java
+++ src/main/java/com/konrad/json/demo/Driver.java
@@ -1,9 +1,11 @@
 package com.konrad.json.demo;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class Driver {
     public static void main(String[] args) {
         try {
-
+            ObjectMapper
         } catch (Exception e) {
             e.printStackTrace();
         }
