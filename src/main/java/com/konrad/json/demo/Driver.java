51eb2aee7ff6db8a1e2ca05e075143ae63d04298
Konrad Bartczak <konradbartczak@onet.pl>
2018-08-30 22:32:15 +0200

    read from json

--- src/main/java/com/konrad/json/demo/Driver.java
+++ src/main/java/com/konrad/json/demo/Driver.java
@@ -2,10 +2,14 @@ package com.konrad.json.demo;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.io.File;
+
 public class Driver {
     public static void main(String[] args) {
         try {
-            ObjectMapper
+            ObjectMapper mapper = new ObjectMapper();
+            Student student = mapper.readValue(new File("data/sample-full.json"), Student.class);
+            System.out.println(student);
         } catch (Exception e) {
             e.printStackTrace();
         }
--- src/main/java/com/konrad/json/demo/Student.java
+++ src/main/java/com/konrad/json/demo/Student.java
@@ -1,10 +1,14 @@
 package com.konrad.json.demo;
 
+import java.util.Arrays;
+
 public class Student {
     private int id;
     private String firstName;
     private String lastName;
     private boolean active;
+    private Address address;
+    private String[] languages;
 
     public Student() {
     }
@@ -39,5 +43,33 @@ public class Student {
 
     public void setActive(boolean active) {
         this.active = active;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public String[] getLanguages() {
+        return languages;
+    }
+
+    public void setLanguages(String[] languages) {
+        this.languages = languages;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", active=" + active +
+                ", address=" + address +
+                ", languages=" + Arrays.toString(languages) +
+                '}';
     }
 }
